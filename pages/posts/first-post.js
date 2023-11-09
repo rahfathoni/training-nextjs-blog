d4396d3e1eb068324c8aceb5cae833bfe956a8bf
fathoni.code <rahmat.fathoni@adidata.co.id>
2023-11-09 10:53:23 +0700

    testing lagi

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -20,7 +20,7 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-      <h5>testing rebuild</h5>
+      <h5>testing rebuild test</h5>
     </Layout>
   );
 }
